19952065af5d695f668818fdb1251d39387befc8
Kyle Grant <95764976+K-MkrOps@users.noreply.github.com>
2023-02-04 19:03:42 -0800

    Create data_processing.py

---
+++ scripts/data_processing.py
@@ -0,0 +1,25 @@
+# Data Processing Script for VariationalFreeEnergyModeling
+
+import numpy as np
+import pandas as pd
+
+class DataProcessing:
+    def __init__(self, input_data, output_data):
+        self.input_data = input_data
+        self.output_data = output_data
+
+    def preprocess(self):
+        # Preprocess the input data and store it in the output_data folder
+        pass
+
+    def normalize(self):
+        # Normalize the input data and store it in the output_data folder
+        pass
+
+    def feature_engineering(self):
+        # Perform feature engineering on the input data and store it in the output_data folder
+        pass
+
+    def feature_selection(self):
+        # Perform feature selection on the input data and store it in the output_data folder
+        pass
